e8cee0df39201e67686daabf16d76c2a04e83e89
Damilka05 <144436727+Damilka05@users.noreply.github.com>
2024-10-27 22:12:19 +0300

    Лабораторная работа 3

---
+++ Лаба 3/lab3_task1.py
@@ -0,0 +1,14 @@
+# TODO Напишите функцию для поиска индекса товара
+def ans(item_list, name):
+    if name in item_list:
+        return item_list.index(name)
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = ans(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
